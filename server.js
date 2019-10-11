c5b89f417926b693666eb180dd1a1f3fb8e68828
Natalia Barriga <nbarrigam@MacBook-Air-de-Natalia.local>
2019-10-11 14:01:20 -0300

    carpetas

---
+++ server.js
@@ -0,0 +1,11 @@
+const express= require('express');
+const app= express();
+const port=5046;
+
+app.use(express.static(__dirname + '/src'));
+app.get('/', (request, response) =>{
+    response.sendFile(__dirname + '/index.html');
+});
+app.listen(port , ()=>{
+    console.log("servidor express funciona")
+});
