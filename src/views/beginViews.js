9cde80e5f460ddbf807de19a8a300448d8347d7e
Natalia Barriga <nbarrigam@MacBook-Air-de-Natalia.local>
2019-10-16 09:29:55 -0300

    registro funcionando

--- src/views/beginViews.js
+++ src/views/beginViews.js
@@ -1,51 +1,71 @@
-export const principal=()=> {
-    const container=document.getElementById("root");
-    container.innerHTML += 
+import {registerUser} from '../models/begin.js'
+import {loginUser} from '../models/begin.js'
+
+export const principal = ()=> {
+    const root = document.getElementById("root");
+    root.innerHTML = 
     `<div class="center">
         <img>
-        <button id="btnRegistry">Registrate</button>
+        <button id="btnRegistry">Regístrate</button>
         <button id="btnLogin">Iniciar Sesión</button>
     </div>`
-    
-    const btnregister=document.getElementById("btnRegistry");
-    btnregister.addEventListener('click', ()=>{
+
+    const btnRegistry=document.getElementById("btnRegistry");
+    btnRegistry.addEventListener('click', ()=>{ 
         register()
     })
-
-    const btnlogin=document.getElementById("btnLogin");
-    btnlogin.addEventListener('click', ()=>{
+    
+    const btnLogin=document.getElementById("btnLogin");
+    btnLogin.addEventListener('click', ()=>{
         login()
     })
 }
 
-export const register=()=> {
-    const container=document.getElementById("root");
-    container.innerHTML = "";
-    container.innerHTML += 
+export const register = () =>{
+    const root =document.getElementById("root");
+    root.innerHTML =
     `<form class="center">
-        <input id="nameRegistry" type="text" placeholder="Ingresa tu Nombre">
-        <input id="mailRegistry" type="email" placeholder="Ingresa tu E-mail">
-        <input id="passwordRegistry" type="password" placeholder="Ingresa tu contraseña">
-        <button id="btnRegistryOk">Registrarse</button>
+    <input id="nameRegistry" type="text" placeholder="Ingresa tu Nombre">
+    <input id="mailRegistry" type="email" placeholder="Ingresa tu E-mail">
+    <input id="passwordRegistry" type="password" placeholder="Ingresa tu contraseña">
+    <button id="btnRegistryOk">Registrarse</button>
     </form>`
 
-    const buttonregister=document.getElementById("btnRegistryOk");
-    buttonregister.addEventListener("click",() =>{
-        console.log("hola");
-        
-        let email=document.getElementById("mailRegistry").value;
-        let password=document.getElementById("passwordRegistry").value;
-        registeruser(email,password);
+    const btnRegistryOk = document.getElementById("btnRegistryOk");
+    btnRegistryOk.addEventListener("click", () => {
+        let nameRegistry = document.getElementById("nameRegistry").value;
+        let mailRegistry = document.getElementById("mailRegistry").value;
+        let passwordRegistry = document.getElementById("passwordRegistry").value;
+        modalRegistry();
+        registerUser(nameRegistry, mailRegistry, passwordRegistry);
     })
 }
 
-export const login=()=> {
+const modalRegistry = () =>{
     const container=document.getElementById("root");
     container.innerHTML = "";
     container.innerHTML += 
+    `<div class="modalContainer">
+    <div class="modalInfo">
+      <h1>¡Muchas Gracias!</h1>
+      <p>Te enviamos un correo para que puedas validar tu cuenta</p>
+    </div>
+    </div>`
+}
+
+export const login = () => {
+    const root =document.getElementById("root");
+    root.innerHTML = 
     `<form class="center">
        <input id="mailLogin" type="email" placeholder="Ingresa tu E-mail">
-       <input id="passwordLogin" type="email" placeholder="Ingresa tu contraseña">
+       <input id="passwordLogin" type="password" placeholder="Ingresa tu contraseña">
        <button id="btnLoginOk">Iniciar Sesión</button>
     </form>`
+
+    const btnLoginOk = document.getElementById("btnLoginOk");
+    btnLoginOk.addEventListener("click", () =>{
+        let mailLogin = document.getElementById("mailLogin").value;
+        let passwordLogin = document.getElementById("passwordLogin").value;
+        loginUser(mailLogin,passwordLogin);
+    })
 }
